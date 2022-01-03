5c6a57645d74fef2cccb8d430595872d873bb3e8
Maxi Ferreira <charca@gmail.com>
2022-01-03 10:37:30 -0800

    Frameless version

---
+++ src/components/Container.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import { fade, fly } from 'svelte/transition';
+
+</script>
+
+<div class="container" transition:fly="{{ y: 20, duration: 250 }}">
+  <header></header>
+  <main>
+    Hello world!
+  </main>
+</div>
+
+<style>
+  .container {
+    width: 350px;
+    height: 600px;
+    border-radius: 6px;
+    box-shadow: 0 0 0 1px rgba(0, 0, 0, .05),
+                0 5px 30px 0 rgba(0, 0, 0, .15),
+                0 3px 3px 0 rgba(0, 0, 0, .05);
+
+    position: fixed;
+    bottom: 120px;
+    right: 40px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  header {
+    background-color: blueviolet;
+    border-radius: 6px 6px 0 0;
+    height: 50px;
+  }
+
+  main {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+  }
+</style>
---
+++ src/components/FAB.svelte
@@ -0,0 +1,25 @@
+<button class="fab" on:click>
+  👆
+</button>
+
+<style>
+  .fab {
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    background-color: blueviolet;
+    font-size: 20px;
+    box-shadow: 0px 4px 7px rgba(0, 0, 0, 0.1);
+    cursor: pointer;
+    margin: 0;
+    border: 0;
+
+    position: fixed;
+    bottom: 40px;
+    right: 40px;
+  }
+
+  .fab:active {
+    background-color: rebeccapurple;
+  }
+</style>
